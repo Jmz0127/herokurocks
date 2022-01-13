a48bfb256015a77c972209460bf2634dfabbfc63
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-13 12:52:02 -0500

    set up server

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ if (process.argv[2] === 'web-49') { //in terminal try node index.js web-49 - thi
 }
 
 console.log(`the user is ${user}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hey there'})
+})
+
+const port = 9000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
