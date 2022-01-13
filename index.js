bc92772585d8cc4dfbee59f13ab2741320ede26a
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-13 17:48:19 -0500

    cleaned up and added some end points

--- index.js
+++ index.js
@@ -3,29 +3,53 @@ const user = process.env.USER;
 // to inject the config var inside of the .env file
 require('dotenv').config();
 
-if (process.argv[2] === 'web-49') {
-	//in terminal try node index.js web-49 - this passes an argument to test
-	console.log('hi');
-} else {
-	console.log('boooo!');
-}
+// if (process.argv[2] === 'web-49') {
+// 	//in terminal try node index.js web-49 - this passes an argument to test
+// 	console.log('hi');
+// } else {
+// 	console.log('boooo!');
+// }
 
 console.log(`the user is ${user}`);
 
 const express = require('express');
-const app = express();
+const server = express();
+const cors = require('cors');
 
-app.get('/hello', (req, res) => {
-	res.json({ message: 'hey there' });
+server.use(express.json());
+server.use(cors());
+
+const port = process.env.PORT || 9000; // heroku wants to set its own port
+server.listen(port, () => {
+	console.log(`listening on port ${port}`);
 });
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
 	res.send(`
 		<h1>Heyo!!</h1>
 	`);
 });
 
-const port = process.env.PORT || 9000; // heroku wants to set its own port
-app.listen(port, () => {
-	console.log(`listening on port ${port}`);
+server.get('/api/users', (req, res) => {
+	res.json({ message: 'here are the users' });
+});
+
+server.get('/api/register', (req, res) => {
+	res.json({ message: 'register here' });
+});
+
+server.get('/api/login', (req, res) => {
+	res.json({ message: 'login here' });
+});
+
+server.use('*', (req, res) => {
+	res.send(`<h1>this is a catch all🧤</h1>`);
+});
+
+server.use((err, req, res, next) => {
+	//eslint-disable-line
+	res.status(500).json({
+		message: err.message,
+		stack: err.stack
+	});
 });
