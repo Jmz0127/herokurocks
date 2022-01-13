64fba40cab445f4b89ddd5afc69af9963e0dcf1c
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-13 13:27:50 -0500

    added app.get to index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const user = process.env.USER;
 const shell = process.env.SHELL;
 
 // to inject the config var inside of the .env file
-require('dotenv').config()
+require('dotenv').config();
 
 if (process.argv[2] === 'web-49') {
 	//in terminal try node index.js web-49 - this passes an argument to test
@@ -18,6 +18,12 @@ const app = express();
 
 app.get('/hello', (req, res) => {
 	res.json({ message: 'hey there' });
+});
+
+app.get('/', (req, res) => {
+	res.send(`
+		<h1>Heyo!!</h1>
+	`);
 });
 
 const port = process.env.PORT || 9000; // heroku wants to set its own port
