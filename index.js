d433673ae47034b06cbdd0cfc1b00bc633167ca4
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-13 12:57:26 -0500

    set up heroku port

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const user = process.env.USER
+const user = process.env.USER;
+const shell = process.env.SHELL;
 
-if (process.argv[2] === 'web-49') { //in terminal try node index.js web-49 - this passes an argument to test
-    console.log('hi')
+if (process.argv[2] === 'web-49') {
+	//in terminal try node index.js web-49 - this passes an argument to test
+	console.log('hi');
 } else {
-    console.log('boooo!')
+	console.log('boooo!');
 }
 
-console.log(`the user is ${user}`)
+console.log(`the user is ${user}`);
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/hello', (req, res) => {
-  res.json({ message: 'hey there'})
-})
+	res.json({ message: 'hey there' });
+});
 
-const port = 9000
+const port = process.env.PORT || 9000; // heroku wants to set its own port
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
-
+	console.log(`listening on port ${port}`);
+});
